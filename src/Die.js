a75debc9ecbf443a060068a6b65a7cdce7a6cfe7
Shane James <shanejames@Shanes-iMac.local>
2021-04-05 20:22:38 -0400

    initial commit. added dice roll logic and minimal css

---
+++ src/Die.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react' 
+import './Die.css' 
+
+class Die extends Component {
+    render () {
+        return (
+            <i className={`fas fa-dice-${this.props.face} ${this.props.rolling ? 'shaking' : ''}`} />
+        )
+    }
+}
+
+export default Die
